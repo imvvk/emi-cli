e17a14f74f002ac372d9f37255ee5a8150c3af6b
kai.wei <kai.wei@qunar.com>
2017-02-27 16:11:24 +0800

    修改模版

--- src/bootstrap/command.js
+++ src/bootstrap/command.js
@@ -12,22 +12,22 @@ module.exports = {
             var template_path = path.join(__emi__.root, "./tmpl/"+template);
             var project_path = path.join(__emi__.cwd, projectName);
 
-            log.info("copy template " +template+ "to project " +  projectName +  "....");
+            log.info("copy template " +template+ " to project " +  projectName +  "....");
             fs.stat(project_path, function (err, result) {
                 if (err) {
                     fse.mkdirsSync(project_path);
                     fse.copySync(template_path, project_path); 
-                    log.info("copy template " +template+ "to project " +  projectName +  " end !");
+                    log.info("copy template [" +template+ "] to project " +  projectName +  " end !");
                 } else {
                     var files = fs.readdirSync(project_path);
                     if (!files.length) {
                         fse.copySync(template_path, project_path); 
-                        log.info("copy template " +template+ "to project " +  projectName +  " end !");
+                        log.info("copy template [" +template+ "] to project " +  projectName +  " end !");
                     } else {
                         inquirer.prompt([{ type :"confirm", "name" : "user_input", message : "project path not empty,  continue ?"}]).then(function (answers) {
                             if (answers.user_input) {
                                 fse.copySync(template_path, project_path); 
-                                log.info("copy template " +template+ "to project " +  projectName +  " end !");
+                                log.info("copy template [" +template+ "] to project " +  projectName +  " end !");
                             } else {
                                 log.info("user cancel");
                             }
