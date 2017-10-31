356776735cd087f9c5762d9d8c2bf739a443f4a9
kai.wei <kai.wei@qunar.com>
2017-10-31 18:56:05 +0800

    add install command

--- src/helpers/download.js
+++ src/helpers/download.js
@@ -4,13 +4,10 @@ var fs = require("fs");
 var fse = require("fs-extra");
 var colors = require('colors');
 var download = require('download');
+var childProcess = require('child_process');
 var envConfig = require("../helpers/config.js");
 
-function downloadGit(gitpath, dest, options ,callback) {
-    if (typeof options === "function") {
-        callback = options;
-        options = {}; 
-    }
+function getGitUrl(gitpath, options) {
     var git =envConfig.get("git");
     if (!git.match(/\/$/)) {
         git +="/";
@@ -19,6 +16,17 @@ function downloadGit(gitpath, dest, options ,callback) {
     if (options.checkout) {
         gitUrl += '?ref='+options.checkout;
     }
+
+    return gitUrl;
+
+}
+
+function downloadGit(gitpath, dest, options ,callback) {
+    if (typeof options === "function") {
+        callback = options;
+        options = {}; 
+    }
+    var gitUrl = getGitUrl(gitpath, options); 
     download(gitUrl, dest , { extract: true, strip: 1, mode: '666', headers: { accept: 'application/zip' } }).then(function (err) {
         if (callback) {
             callback(); 
@@ -45,8 +53,28 @@ function replaceProjectName(dest, name, reg) {
     }
 }
 
+function npmGitInstall(gitpath, options) {
+    if (typeof options === "function") {
+        callback = options;
+        options = {}; 
+    }
+    var gitUrl = getGitUrl(gitpath, options); 
+    
+    log.info("command : npm install -s "+ gitUrl);
+    var proc = childProcess.exec('npm install -s '+ gitUrl, function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        log.info("install completed ");
+    });
+    proc.stdout.pipe(process.stdout);
+    proc.stderr.pipe(process.stderr);
+}
+
 module.exports = {
     gitclone :  downloadGit,
-    replaceProjectName : replaceProjectName
+    replaceProjectName : replaceProjectName,
+    npmGitInstall : npmGitInstall
 }
  
