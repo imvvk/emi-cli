0baac4a4c544c0e77940ae198686cc156f5d9087
kai.wei <kai.wei@qunar.com>
2017-11-06 14:09:22 +0800

    fix output merge bug && add dllReferenceSync in  emiUitls

--- src/bootstrap/globalUtils.js
+++ src/bootstrap/globalUtils.js
@@ -3,7 +3,12 @@
  * use in emi.config.js
  ***/
 
-var cssLoader = require("../compiler/utils/cssLoaders.js");
+const cssLoader = require("../compiler/utils/cssLoaders.js");
+const fs = require('fs');
+const path = require('path');
+const request = require('sync-request');
+const webpack = require('webpack');
+
 
 var Utils = {
     
@@ -30,8 +35,38 @@ var Utils = {
 
     getEnv : function () {
         return __emi__.env; 
-    }
+    },
+    dllReferenceSync : function dllSync(uri, basedir, options) {
+        if (!options && Object.prototype.toString.call(basedir) === '[object Object]') {
+            options = basedir;
+            basedir = null;
+        } 
+        var cwd = basedir || __emi__.cwd || process.cwd();
+        var manifest, content = '';
+        options = options || {};
+        try {
+            if (uri.match(/^https?:\/\//)) {
+                var res = request('GET', uri);
+                content = res.getBody();
+            } else {
+                if (!path.isAbsolute(uri)) {
+                    uri = path.join(cwd, uir);
+                }
+                content = fs.readFileSync(uri,'utf-8');
+            }
 
+            if (content) {
+                manifest = JSON.parse(content);
+                return new webpack.DllReferencePlugin(Object.assign({},options, {
+                    context : cwd,
+                    manifest : manifest
+                }));
+            }
+        } catch(e) {
+            console.log('error uri: ', uri);
+            console.log('error :', e);
+        }
+    } 
 }
 
 module.exports = Utils;
